a07cf120d5e5c1fed00b8e8ef73d732f77ea3e4e
Terence Chow <terence.chow.1986@gmail.com>
2014-09-18 21:27:24 -0400

    many changes

--- app/assets/javascripts/static_pages.js
+++ app/assets/javascripts/static_pages.js
@@ -5,11 +5,14 @@
 
 
 $(document).ready(function() {
+  
   var s;
-  s = skrollr.init({
-    forceHeight: false
-  });
-  skrollr.menu.init(s);
+  if(!(/Android|iPhone|iPad|iPod|BlackBerry|Windows Phone/i).test(navigator.userAgent || navigator.vendor || window.opera)){
+    s = skrollr.init({
+      forceHeight: false
+    });
+    skrollr.menu.init(s);
+  }
   available_space = $(".project-description").width();
   $("#image_1").load(function() {
     width_of_image = $("#image_1").width();
@@ -30,63 +33,62 @@ $(document).ready(function() {
   });
 
   $(window).resize(function(){
-    if (clicked >0){
-      $("#image_1").animate({
-        left: "+=" + clicked * space_to_move
-      }, 800);
-      $("#image_2").animate({
-        left: "+=" + clicked * space_to_move
-      }, 800);
-      $("#image_3").animate({
-        left: "+=" + clicked * space_to_move
-      }, 800);
-      $("#image_4").animate({
-        left: "+=" + clicked * space_to_move
-      }, 800);
-      $("#image_5").animate({
-        left: "+=" + clicked * space_to_move
-      }, 800);
-      $("#image_6").animate({
-        left: "+=" + clicked * space_to_move
-      }, 800);
-      $("#image_7").animate({
-        left: "+=" + clicked * space_to_move
-      }, 800);
-      $("#image_8").animate({
-        left: "+=" + clicked * space_to_move
-      }, 800);
-      $("#image_9").animate({
-        left: "+=" + clicked * space_to_move
-      }, 800);
-      $("#image_10").animate({
-        left: "+=" + clicked * space_to_move
-      }, 800);
-      window.clicked = 0;
-      $("#left-arrow").fadeTo(300,0,function(){
-        $("#left-arrow").css("visibility","hidden");  
-      });
-      $("#right-arrow").css("visibility","visible");
-      $("#right-arrow").fadeTo(300,1);
-    }
-    available_space = $(".project-description").width();
-    width_of_image = $("#image_1").width();
-    number_can_fit = Math.floor(available_space / width_of_image);
-    if (number_can_fit >1){
-      margin_to_add = (available_space - number_can_fit * width_of_image) / (number_can_fit -1);
-      window.number_of_clicks = Math.ceil(10 / number_can_fit);
-      window.number_of_clicks = (window.number_of_clicks == 1) ? 1: window.number_of_clicks -1;
-      window.space_to_move = available_space + margin_to_add;
-      $("#icebreaker .project-screenshots").children().css("margin-right", margin_to_add);  
-    } else {
-      margin_to_add = (available_space - width_of_image)/2;
-      $("#icebreaker .project-screenshots").children().css("margin-right", margin_to_add);
-      $("#icebreaker .project-screenshots").children().css("margin-left", margin_to_add);
-      window.number_of_clicks = 9;
-      window.space_to_move = margin_to_add*2 + width_of_image;
-    }
-
+    console.log("i'm resizing")
+      if (clicked >0){
+        $("#image_1").animate({
+          left: "+=" + clicked * space_to_move
+        }, 800);
+        $("#image_2").animate({
+          left: "+=" + clicked * space_to_move
+        }, 800);
+        $("#image_3").animate({
+          left: "+=" + clicked * space_to_move
+        }, 800);
+        $("#image_4").animate({
+          left: "+=" + clicked * space_to_move
+        }, 800);
+        $("#image_5").animate({
+          left: "+=" + clicked * space_to_move
+        }, 800);
+        $("#image_6").animate({
+          left: "+=" + clicked * space_to_move
+        }, 800);
+        $("#image_7").animate({
+          left: "+=" + clicked * space_to_move
+        }, 800);
+        $("#image_8").animate({
+          left: "+=" + clicked * space_to_move
+        }, 800);
+        $("#image_9").animate({
+          left: "+=" + clicked * space_to_move
+        }, 800);
+        $("#image_10").animate({
+          left: "+=" + clicked * space_to_move
+        }, 800);
+        window.clicked = 0;
+        $("#left-arrow").fadeTo(300,0,function(){
+          $("#left-arrow").css("visibility","hidden");  
+        });
+        $("#right-arrow").css("visibility","visible");
+        $("#right-arrow").fadeTo(300,1);
+      }
+      available_space = $(".project-description").width();
+      width_of_image = $("#image_1").width();
+      number_can_fit = Math.floor(available_space / width_of_image);
+      if (number_can_fit >1){
+        margin_to_add = (available_space - number_can_fit * width_of_image) / (number_can_fit -1);
+        window.number_of_clicks = Math.ceil(10 / number_can_fit);
+        window.number_of_clicks = (window.number_of_clicks == 1) ? 1: window.number_of_clicks -1;
+        window.space_to_move = available_space + margin_to_add;
+        $("#icebreaker .project-screenshots").children().css("margin-right", margin_to_add);  
+      } else {
+        margin_to_add = (available_space - width_of_image)/2;
+        $("#icebreaker .project-screenshots").children().css("margin-right", margin_to_add);
+        $("#icebreaker .project-screenshots").children().css("margin-left", margin_to_add);
+        window.number_of_clicks = 9;
+        window.space_to_move = margin_to_add*2 + width_of_image;
+      }
   });
-
 });
 
 
